ccd253da9b2061463e46c11cef617e5ee2be9a71
rohit20001221 <rohit20001221@gmail.com>
2021-09-07 23:07:39 +0530

    unsubscribe message

--- pubsub/consumers.py
+++ pubsub/consumers.py
@@ -1,25 +1,26 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 
-class MqttBrokerConsumer(AsyncWebsocketConsumer):
-    
-    def decode_message(self, text):
-        return json.loads(text)
-    
-    def encode_message(self, data):
-        return json.dumps(data)
+class MqttBrokerConsumer(AsyncJsonWebsocketConsumer):
     
     # this method is called when websocket connection is created
     async def connect(self):
         await self.accept()
     
+    
     # this method is called when websocket receives a message
-    async def receive(self, text_data):
-        data = self.decode_message(text_data)
+    async def receive_json(self, content):
+        data = content
         
         if 'subscribe' in data:
             await self.channel_layer.group_add(
                 data['subscribe'],
+                self.channel_name
+            )
+            
+        if 'unsubscribe' in data:
+            await self.channel_layer.group_discard(
+                data['unsubscribe'],
                 self.channel_name
             )
         
@@ -28,15 +29,15 @@ class MqttBrokerConsumer(AsyncWebsocketConsumer):
                 data['publish']['topic'],
                 {
                     'type': 'publish.message',
-                    'message': self.encode_message({
+                    'message': {
                         'message': data['publish']['message'],
                         'topic': data['publish']['topic']
-                    })
+                    }
                 }
             )
     
     async def publish_message(self, event):
-        await self.send(self.encode_message(event['message']))
+        await self.send_json(event['message'])    
     
     # this function is called when websocket connection is closed
     async def disconnect(self, code):
